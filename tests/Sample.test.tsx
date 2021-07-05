765c08678b68dc68db087f7cd9136241000a49bb
Tom Bazarnik <tommywalkie@gmail.com>
2021-07-05 10:03:03 +0200

    Trying without all the React stuff

--- tests/Sample.test.tsx
+++ tests/Sample.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { test } from "uvu";
 import * as assert from "uvu/assert";
-import * as ENV from "./setup";
+// import * as ENV from "./setup";
 import { render as renderJSX, screen } from "@testing-library/react";
 import { Processor } from "windicss/lib";
 import { HTMLParser } from "windicss/utils/parser";
@@ -16,7 +16,7 @@ test.after(() => {
 
 test.before.each(() => {
   console.log('>> BEFORE');
-  ENV.reset();
+  // ENV.reset();
 });
 
 test.after.each(() => {
@@ -46,6 +46,7 @@ test('sum', () => {
 	assert.is(1 + 1, 2);
 });
 
+/*
 test('render a React component', () => {
 	const Sample: React.FC = () => {
 		return <div>Hello world</div>
@@ -62,5 +63,6 @@ test('render a React component and register WindiCSS styles', () => {
 	assert.ok(screen.getByText('Hello world'));
   assert.is(document.head.innerHTML, '<style>.px-2{padding-left:0.5rem;padding-right:0.5rem}</style>');
 });
+*/
 
 test.run();
